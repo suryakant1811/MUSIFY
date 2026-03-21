26753f844fcb95a6b76d7e16bddf5d9921ad0b25
suryadwd <surajdwivedi644@gmail.com>
2026-03-21 14:17:14 +0530

    update

--- app/frontend/src/lib/axios.ts
+++ app/frontend/src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: "https://musify-1-nxyi.onrender.com",
-  
+  baseURL: "https://musify-1-nxyi.onrender.com/api",
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
   }
